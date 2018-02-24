bb57147feb5b9958aefdaf05e64ede9da12c4611
alexruilli <alexruiz@jardesign.me>
2018-02-24 12:08:00 -0600

    Bot API - Comandos
    NodeJS, Bot API, Primeros Comandos

---
+++ bot.js
@@ -0,0 +1,42 @@
+process.env["NTBA_FIX_319"] = 1;
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '540566105:AAHl5LaX1fdGqlN44WsNjNb9dK6AAC8RT1A';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+//direccion comando
+
+bot.onText(/\/direccion/, (msg) => {
+
+bot.sendMessage(msg.chat.id, "Local primero del primer centrocomercial");
+
+});
+
+//algunas veces - lee y responde
+bot.on('message', (msg) => {
+
+var algunas= "algunas veces";
+if (msg.text.toString().toLowerCase().indexOf(algunas) === 0) {
+bot.sendMessage(msg.chat.id,"No te conosco");
+}
+
+});
+
+//Mayor que numeros
+
+bot.onText(/\/mayor/, (msg) => {
+
+bot.sendMessage(msg.chat.id, "es mayor");
+
+});
+
+//Enviar Meme
+
+bot.onText(/\/unmeme/, (msg) => {
+
+bot.sendPhoto(msg.chat.id,"http://blogs.publico.es/strambotic/files/2016/12/okkikkojpg_EDIIMA20150304_0426_13.jpg",{caption : "Por andar pidiendo momos"} );
+
+});
